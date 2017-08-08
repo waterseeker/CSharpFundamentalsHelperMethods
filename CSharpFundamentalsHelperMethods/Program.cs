c3c674175a504872ad8740e60a456b2b5cc50587
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-08 10:34:33 -0500

    added static to the superSecretFormula method

--- CSharpFundamentalsHelperMethods/Program.cs
+++ CSharpFundamentalsHelperMethods/Program.cs
@@ -6,9 +6,16 @@ namespace CSharpFundamentalsHelperMethods
     {
         static void Main(string[] args)
         {
+            string myValue = superSecretFormula();
+            Console.WriteLine(myValue);
+            Console.ReadLine();
         }
 
-
+        private static string superSecretFormula()
+        {
+            //some cool stuff here
+            return "Hello World!";
+        }
 
     }
 }
